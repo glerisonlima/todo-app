449b75cd8d1b6c2992010e273d17049b090794f6
glerisonlima <glerisonlima@gmail.com>
2019-04-24 00:57:26 -0300

    adicionado formulario

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from   './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component{
+    
+    constructor(props){
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value })
+    }
+
+    handleAdd(){
+        console.log(this)
+    } 
+    
     render(){
         return(
             <div>
-                <h1>Todo</h1>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm description={this.state.description} handleAdd={this.handleAdd}/>
+                <TodoList />
             </div>
         )
     }
